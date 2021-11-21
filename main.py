180cf5a015e2b0e3abbebe43c9f783e750947e20
dooke <58890963+dzuqe@users.noreply.github.com>
2021-11-21 16:13:57 +0200

    use main file as driver

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import os
+from node import Node
+from clerk import Clerk
+
 print("hi")
+node = Node(os.environ["PURESTAKE_API_KEY"], 'https://mainnet-algorand.api.purestake.io/ps2')
+choice="clerk"
+
+if choice == "clerk":
+    clerk = Clerk(os.environ['MARC'])
+    print(clerk)
+    #clerk.transfer(binance_acc, amount, node.client)
+else:
+    print("help")
