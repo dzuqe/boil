a0e0bd71632b350c5f0e82f89200cc4ada9d3ca6
dooke <58890963+dzuqe@users.noreply.github.com>
2021-11-21 18:05:52 +0200

    read command line options

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
+#!/usr/bin/env python
 import os
+import sys
 from boil import Boil
 
-boil = Boil()
+if len(sys.argv) <= 1:
+    print("help")
+else:
+    choice = sys.argv[1]
+    boil = Boil()
 
+    if choice == "clerk":
+        print("clerk")
+        print(boil.clerk)
 
+    elif choice == "asset":
+        print("asset")
+        print(boil.asset)
+
+    elif choice == "app":
+        print("app")
+        print(boil.app)
+
+    elif choice == "account":
+        print("account")
+        print(boil.account)
+
+    else:
+        print("Error: Unknown option")
